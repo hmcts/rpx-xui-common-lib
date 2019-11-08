9c11c01104c976b2ae5a50169ce6ad4a18385a7f
Ronald Mansveld <ronald.mansveld@atos.net>
2019-11-08 16:24:04 +0000

    Make module safe to import in lazy loaded modules now we have components as well

--- projects/exui-common-lib/src/lib/exui-common-lib.module.ts
+++ projects/exui-common-lib/src/lib/exui-common-lib.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { Inject, InjectionToken, ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { AcceptTermsAndConditionsComponent } from './components/accept-terms-and-conditions/accept-terms-and-conditions.component';
 import { TcDisplayHtmlComponent } from './components/terms-and-conditions/tc-display/tc-display-html/tc-display-html.component';
 import { TcDisplayPlainComponent } from './components/terms-and-conditions/tc-display/tc-display-plain/tc-display-plain.component';
 import { TermsAndConditionsComponent } from './components/terms-and-conditions/terms-and-conditions.component';
+import { GoogleAnalyticsService } from './services/google-analytics/google-analytics.service';
 import { windowProvider, windowToken } from './window';
+
+export const COMMON_LIB_ROOT_GUARD = new InjectionToken<void>('COMMON_LIB_ROOT_GUARD');
 
 @NgModule({
   declarations: [
@@ -24,4 +27,35 @@ import { windowProvider, windowToken } from './window';
     AcceptTermsAndConditionsComponent
   ]
 })
-export class ExuiCommonLibModule { }
+export class ExuiCommonLibModule {
+
+  constructor(@Optional() @Inject(COMMON_LIB_ROOT_GUARD) public guard: any) {}
+
+  public static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: ExuiCommonLibModule,
+      providers: [
+        GoogleAnalyticsService,
+        {
+          provide: COMMON_LIB_ROOT_GUARD,
+          useFactory: provideForRootGuard,
+          deps: [[GoogleAnalyticsService, new Optional(), new SkipSelf()]]
+        }
+      ]
+    };
+  }
+
+  public static forChild(): ModuleWithProviders {
+    return {
+      ngModule: ExuiCommonLibModule,
+      providers: []
+    };
+  }
+}
+
+export function provideForRootGuard(service: GoogleAnalyticsService): any {
+  if (service) {
+    throw new Error(`ExuiCommonLibModule.forRoot() called twice. Lazy loaded modules should use forChild() instead.`);
+  }
+  return 'guarded';
+}
