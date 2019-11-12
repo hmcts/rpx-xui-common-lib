d9a6d3a36a6e971d553d3883afb9419c84b935ac
Ronald Mansveld <ronald.mansveld@atos.net>
2019-11-12 10:54:17 +0000

    Fix tests

--- projects/exui-common-lib/src/lib/components/terms-and-conditions/terms-and-conditions.component.spec.ts
+++ projects/exui-common-lib/src/lib/components/terms-and-conditions/terms-and-conditions.component.spec.ts
@@ -1,14 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TCDocument } from '../../models';
+import { TcDisplayHtmlComponent } from './tc-display/tc-display-html/tc-display-html.component';
+import { TcDisplayPlainComponent } from './tc-display/tc-display-plain/tc-display-plain.component';
 import { TermsAndConditionsComponent } from './terms-and-conditions.component';
 
 describe('TermsAndConditionsComponent', () => {
   let component: TermsAndConditionsComponent;
   let fixture: ComponentFixture<TermsAndConditionsComponent>;
 
+  const testData: TCDocument = {
+    content: 'Test',
+    version: 1,
+    mimeType: 'text/plain'
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TermsAndConditionsComponent ]
+      declarations: [ TermsAndConditionsComponent, TcDisplayPlainComponent, TcDisplayHtmlComponent ]
     })
     .compileComponents();
   }));
@@ -16,6 +24,7 @@ describe('TermsAndConditionsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TermsAndConditionsComponent);
     component = fixture.componentInstance;
+    component.document = testData;
     fixture.detectChanges();
   });
 
