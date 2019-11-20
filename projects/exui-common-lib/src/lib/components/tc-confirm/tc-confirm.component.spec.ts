110c114b0efa648976bbaf9c05701cc324fa9797
Ronald Mansveld <ronald.mansveld@gmail.com>
2019-11-20 16:48:44 +0000

    Fix tests

--- projects/exui-common-lib/src/lib/components/tc-confirm/tc-confirm.component.spec.ts
+++ projects/exui-common-lib/src/lib/components/tc-confirm/tc-confirm.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { TcConfirmComponent } from './tc-confirm.component';
+
 
 describe('TcConfirmComponent', () => {
   let component: TcConfirmComponent;
@@ -8,9 +9,10 @@ describe('TcConfirmComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TcConfirmComponent ]
+      declarations: [TcConfirmComponent],
+      imports: [RouterTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
