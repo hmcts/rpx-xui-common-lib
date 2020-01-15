a895f02c8ac20a4aeb4c0dafd11063fd2978ec2f
Franco Zanki <fazanki@users.noreply.github.com>
2020-01-15 16:58:57 +0000

    Bug/eui 1138 session bug fix (#15)
    * adding forgotten exports to public_api.ts
    
    * bumping up version

--- projects/exui-common-lib/src/lib/components/public_api.ts
+++ projects/exui-common-lib/src/lib/components/public_api.ts
@@ -1,3 +1,4 @@
 export * from './terms-and-conditions/terms-and-conditions.component';
 export * from './tc-confirm/tc-confirm.component';
 export * from './contact-details/contact-details.component';
+export * from './hmcts-session-dialog/hmcts-session-dialog.component';
