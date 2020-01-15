a2db712003bd6deac806c2a3bb5798c1e540c842
fazanki <fazanki@gmail.com>
2020-01-15 13:53:39 +0000

    fixing tests

--- projects/exui-common-lib/src/lib/services/idle/idle.services.spec.ts
+++ projects/exui-common-lib/src/lib/services/idle/idle.services.spec.ts
@@ -1,13 +1,10 @@
-import { inject, TestBed } from '@angular/core/testing';
-import {StoreModule, Store} from '@ngrx/store';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { IdleService } from './idle.services';
-import {Idle, IdleExpiry} from '@ng-idle/core';
-import {Keepalive} from '@ng-idle/keepalive';
+import { inject, TestBed } from '@angular/core/testing';
+import { Idle} from '@ng-idle/core';
+import { Keepalive } from '@ng-idle/keepalive';
 import { of } from 'rxjs';
-import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import * as fromRoot from '../../store';
-import {IdleConfigModel} from '../../models/idle-config.model';
+import { IdleConfigModel} from '../../models/idle-config.model';
+import { IdleService } from './idle.services';
 
 describe('Idle Services', () => {
 
@@ -19,39 +16,28 @@ describe('Idle Services', () => {
     'watch'
   ]);
 
-  mockIdle.onIdleEnd = { pipe: (param) => of('something') };
+  mockIdle.onIdleEnd = { pipe: () => of('something') };
   mockIdle.onTimeout = of('something');
   mockIdle.onIdleStart = of('something');
-  mockIdle.onTimeoutWarning = { pipe: (param) => of('something') };
-
-  let store: MockStore<fromRoot.State>;
-  let spyOnDispatchToStore = jasmine.createSpy();
-  let spyOnPipeToStore = jasmine.createSpy();
+  mockIdle.onTimeoutWarning = { pipe: () => of('something') };
 
   const mockKeepAlive = jasmine.createSpyObj('KeepInterval', [
     'interval'
   ]);
 
-  mockKeepAlive.onPing = { pipe: (param) => of('something') };
+  mockKeepAlive.onPing = { pipe: () => of('something') };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        StoreModule.forRoot({}),
         HttpClientTestingModule
       ],
       providers: [
         { provide: Idle, useValue: mockIdle },
         { provide: Keepalive, useValue: mockKeepAlive },
         IdleService,
-        provideMockStore(),
       ]
     });
-
-    store = TestBed.get(Store);
-    spyOnDispatchToStore = spyOn(store, 'dispatch').and.callThrough();
-    spyOnPipeToStore = spyOn(store, 'pipe').and.returnValue(of('something'));
-
   });
 
 
