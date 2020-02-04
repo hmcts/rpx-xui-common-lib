68c040d78936c53815a2ef1e9932131ef74b1dde
Ronald Mansveld <ronald.mansveld@atos.net>
2020-02-04 10:39:09 +0000

    Feature/let directive (#19)
    * Avoid use of unknown as this isn't compatible with older versions of TS

--- projects/exui-common-lib/src/lib/directives/let/let.directive.ts
+++ projects/exui-common-lib/src/lib/directives/let/let.directive.ts
@@ -1,6 +1,6 @@
 import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
-export class LetContext<T = unknown> {
+export class LetContext<T = any> {
     public $implicit: T = null;
     public ngLet: T = null;
 }
@@ -8,7 +8,7 @@ export class LetContext<T = unknown> {
 @Directive({
     selector: '[xuilibLet]'
 })
-export class LetDirective<T = unknown> {
+export class LetDirective<T = any> {
     private readonly context: LetContext<T> = new LetContext<T>();
 
     constructor(private readonly viewContainer: ViewContainerRef, templateRef: TemplateRef<LetContext<T>>) {
