2429f959454b49d75c8abb232694669a00ae519e
Raymond Liang <ray.liang@atos.net>
2020-07-24 09:11:32 +0100

    EUI-2072-call-real-apis:add case type id and only show access block when there is change

--- projects/exui-common-lib/src/lib/components/selected-case-confirm/selected-case-confirm.component.spec.ts
+++ projects/exui-common-lib/src/lib/components/selected-case-confirm/selected-case-confirm.component.spec.ts
@@ -33,4 +33,39 @@ describe('SelectedCaseConfirmComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not show user access block', () => {
+    expect(component.showUserAccessBlock()).toBeFalsy();
+    fixture.detectChanges();
+    const userAccessBlock = fixture.debugElement.nativeElement.querySelector('#user-access-block');
+    expect(userAccessBlock).toBeNull();
+  });
+
+  it('should show user access block', () => {
+    component.sharedCase = {
+      caseId: 'C111111',
+      caseTitle: 'Share a case',
+      sharedWith: [{
+        idamId: 'U111111',
+        firstName: 'James',
+        lastName: 'Priest',
+        email: 'james.priest@test.com'
+      }],
+      pendingUnshares: [{
+        idamId: 'U111111',
+        firstName: 'James',
+        lastName: 'Priest',
+        email: 'james.priest@test.com'
+      }]
+    };
+    expect(component.showUserAccessBlock()).toBeTruthy();
+    fixture.detectChanges();
+    const userAccessBlock = fixture.debugElement.nativeElement.querySelector('#user-access-block');
+    expect(userAccessBlock).toBeTruthy();
+  });
+
+  afterEach(() => {
+    component.sharedCase = null;
+    fixture.destroy();
+  });
 });
--- projects/exui-common-lib/src/lib/models/case-share.model.ts
+++ projects/exui-common-lib/src/lib/models/case-share.model.ts
@@ -3,6 +3,7 @@ import { UserDetails } from './user-details.model';
 export interface SharedCase {
   caseId: string;
   caseTitle: string;
+  caseTypeId?: string;
   roles?: string[];
   sharedWith?: UserDetails[];
   pendingShares?: UserDetails[];
