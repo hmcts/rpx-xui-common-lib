dc60d31c760708a971a326065a38d730afc013a2
Ronald Mansveld <ronald.mansveld@atos.net>
2020-11-23 00:09:28 +0000

    Add generic guard for featuretoggles

---
+++ projects/exui-common-lib/src/lib/services/feature-toggle/feature-toggle.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, UrlTree } from '@angular/router';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { FeatureToggleService } from './feature-toggle.service';
+
+@Injectable()
+export class FeatureToggleGuard implements CanActivate {
+
+    public constructor(
+        private readonly featureToggleService: FeatureToggleService,
+        private readonly router: Router
+    ) {}
+
+    /**
+     * Usage: Add the FeatureToggleGuard as the Guard for a route, and provide the following
+     * in the data array for the route:
+     * - needsFeaturesEnabled: An array of feature keys that need to be enabled for this route
+     * - featureDisabledRedirect: the URL to redirect to when the this route is not accessible due to disabled features
+     * @param route Automatically provided by Angular
+     */
+    public canActivate(route: ActivatedRouteSnapshot): Observable<boolean | UrlTree> {
+        return combineLatest(...(route.data.needsFeaturesEnabled as string[]).map(
+                feature => this.featureToggleService.getValueOnce<boolean>(feature, false)
+        )).pipe(
+            map(featureStatuses => featureStatuses.every(status => status)),
+            map(status => status || this.router.parseUrl(route.data.featureDisabledRedirect as string))
+        );
+    }
+}
