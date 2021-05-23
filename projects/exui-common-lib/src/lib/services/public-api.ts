602cac34b60f1ac7089c360e8e6c86371c7cefa9
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2021-05-23 14:18:42 +0100

    EUI-3848 fixing conflict merge mess

--- projects/exui-common-lib/src/lib/services/public-api.ts
+++ projects/exui-common-lib/src/lib/services/public-api.ts
@@ -13,5 +13,4 @@ export * from './manage-session/manage-session.services';
 export * from './role-guard/role.guard';
 export * from './role-guard/role.service';
 export * from './timeout-notifications/timeout-notifications.service';
-export * from './loading/loading.service';
 export * from './cookie/cookie.service';
