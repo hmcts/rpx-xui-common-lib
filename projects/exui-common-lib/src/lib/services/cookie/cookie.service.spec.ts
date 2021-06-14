6756ff96d0c42beb7848a4833f64ec049af9dfc0
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2021-06-14 15:01:25 +0100

    EUI-3842 trial for preview

--- projects/exui-common-lib/src/lib/services/cookie/cookie.service.spec.ts
+++ projects/exui-common-lib/src/lib/services/cookie/cookie.service.spec.ts
@@ -42,7 +42,7 @@ describe('CookieService', () => {
     const result = cookieService.checkCookie('user');
     expect(result).toBeTruthy();
     cookieService.deleteCookie('user');
-    expect(mockDocument.cookie).toBe(`user=; expires=${new Date(0)}`);
+    expect(mockDocument.cookie).toBe(`user=; path=/; expires=${new Date(0)}`);
   });
 
   it('should delete a cookie by partial match', () => {
@@ -50,6 +50,6 @@ describe('CookieService', () => {
     const result = cookieService.checkCookie('user_123');
     expect(result).toBeTruthy();
     cookieService.deleteCookieByPartialMatch('user_');
-    expect(mockDocument.cookie).toBe(`user_123=; expires=${new Date(0)}`);
+    expect(mockDocument.cookie).toBe(`user_123=; path=/; expires=${new Date(0)}`);
   });
 });
