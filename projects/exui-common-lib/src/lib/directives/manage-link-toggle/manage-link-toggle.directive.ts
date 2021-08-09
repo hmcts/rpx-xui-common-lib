8464de37c5225140cfd56edab81bcb698d2b3e05
kuda <ku-da@hotmail.co.uk>
2021-08-09 14:29:07 +0100

    change the property name

--- projects/exui-common-lib/src/lib/directives/manage-link-toggle/manage-link-toggle.directive.ts
+++ projects/exui-common-lib/src/lib/directives/manage-link-toggle/manage-link-toggle.directive.ts
@@ -1,14 +1,17 @@
-import {Directive, ElementRef, Input} from '@angular/core';
+import {Directive, ElementRef, Input, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[xuilibManageLinkToggle]'
 })
-export class ManageLinkToggleDirective {
-  @Input() public invisible: boolean = true;
+export class ManageLinkToggleDirective implements OnInit {
+  @Input() public hidden: boolean = true;
 
   constructor(private readonly element: ElementRef) {
+  }
+
+  public ngOnInit(): void {
     const host = this.element.nativeElement as HTMLElement;
-    if (this.invisible) {
+    if (this.hidden) {
       host.style.display = 'none';
     }
   }
