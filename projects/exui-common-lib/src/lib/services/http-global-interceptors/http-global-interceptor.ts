9098e6e740eda5b8ae47cab0398a4aa57869a71d
waynepHMCTS <wayne.porter@hmcts.net>
2021-09-21 14:55:14 +0100

    lint interceptor

--- projects/exui-common-lib/src/lib/services/http-global-interceptors/http-global-interceptor.ts
+++ projects/exui-common-lib/src/lib/services/http-global-interceptors/http-global-interceptor.ts
@@ -1,35 +1,31 @@
-  import {
-    HttpHandler,
-    HttpRequest,
-    HttpEvent,
-    HttpInterceptor,
-    HttpErrorResponse,
-  } from '@angular/common/http';
-  import { catchError } from 'rxjs/operators';
-  import { Injectable } from '@angular/core';
-  import { Observable, throwError } from 'rxjs';
-  
-  @Injectable()
-  export class HttpGlobalInterceptor implements HttpInterceptor {
-    constructor() {}
-  
-    intercept(
-      request: HttpRequest<any>,
-      next: HttpHandler
-    ): Observable<HttpEvent<any>> {
-      return next.handle(request).pipe(
-        catchError((error: HttpErrorResponse) => {
-          let errorMessage = '';
-          if (error.error instanceof ErrorEvent) {
-            // client-side error
-            errorMessage = error.error.message;
-          } else {
-            // server-side error
-            errorMessage = error.message;
-          }
-          return throwError(errorMessage);
-        })
-      ) as Observable<HttpEvent<any>>;
-    }
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';  
+@Injectable()
+export class HttpGlobalInterceptor implements HttpInterceptor {  
+  public intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+          // client-side error
+          errorMessage = error.error.message;
+        } else {
+          // server-side error
+          errorMessage = error.message;
+        }
+        return throwError(errorMessage);
+      })
+    ) as Observable<HttpEvent<any>>;
   }
-  
+}
