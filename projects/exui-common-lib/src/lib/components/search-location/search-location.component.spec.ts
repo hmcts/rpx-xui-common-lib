7e857f076570e8f2d25ecc10fecf3741a1c1cc10
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2021-12-01 16:05:00 +0000

    f remove

--- projects/exui-common-lib/src/lib/components/search-location/search-location.component.spec.ts
+++ projects/exui-common-lib/src/lib/components/search-location/search-location.component.spec.ts
@@ -7,7 +7,7 @@ import { of } from 'rxjs';
 import { LocationService } from '../../services/locations/location.service';
 import { SearchLocationComponent } from './search-location.component';
 
-fdescribe('SearchLocationComponent', () => {
+describe('SearchLocationComponent', () => {
   let component: SearchLocationComponent;
   let fixture: ComponentFixture<SearchLocationComponent>;
   const searchFilterServiceMock = jasmine.createSpyObj('LocationService', ['getAllLocations']);
