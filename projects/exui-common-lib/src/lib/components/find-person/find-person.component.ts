e34c03bd297414fbc7032ef5bb77257f645ad619
sakanam <sonny.akanam@atos.net>
2022-01-25 10:22:25 +0000

    find person styling fix

--- projects/exui-common-lib/src/lib/components/find-person/find-person.component.ts
+++ projects/exui-common-lib/src/lib/components/find-person/find-person.component.ts
@@ -106,8 +106,8 @@ export class FindPersonComponent implements OnInit, OnChanges {
 
   public getDisplayName(selectedPerson: Person): string {
     if (selectedPerson.domain === PersonRole.JUDICIAL && selectedPerson.knownAs) {
-      return `${selectedPerson.knownAs}(${selectedPerson.email})`;
+      return `${selectedPerson.knownAs} (${selectedPerson.email})`;
     }
-    return selectedPerson.email ? `${selectedPerson.name}(${selectedPerson.email})` : selectedPerson.name;
+    return selectedPerson.email ? `${selectedPerson.name} (${selectedPerson.email})` : selectedPerson.name;
   }
 }
