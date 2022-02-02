235bb02bb9e4eecb8d81d85658c03ecf7a3398ef
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2022-02-02 14:44:09 +0000

    Epimms changes (#231)
    * Epimms changes
    
    * Update version
    
    * Change court_name to venue_name

--- projects/exui-common-lib/src/lib/services/locations/location.service.ts
+++ projects/exui-common-lib/src/lib/services/locations/location.service.ts
@@ -1,7 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
-import {LocationByEPIMSModel} from '../../models/location.model';
+import {LocationByEPIMMSModel} from '../../models/location.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,9 +16,9 @@ export class LocationService {
    * @param serviceIds: SSCS | SSCS,IA split with ','
    * @param locationType: optional | hearing | case_management
    * @param searchTerm: any search term for postcode | site name | venue name |court name | court address etc.
-   * @return Observable<LocationByEPIMSModel[]>: Array of locationModel in Observable
+   * @return Observable<LocationByEPIMMSModel[]>: Array of locationModel in Observable
    */
-  public getAllLocations(serviceIds: string, locationType: string, searchTerm: string): Observable<LocationByEPIMSModel[]> {
-    return this.http.get<LocationByEPIMSModel[]>(`api/locations/getLocations?serviceIds=${serviceIds}&locationType=${locationType}&searchTerm=${searchTerm}`);
+  public getAllLocations(serviceIds: string, locationType: string, searchTerm: string): Observable<LocationByEPIMMSModel[]> {
+    return this.http.get<LocationByEPIMMSModel[]>(`api/locations/getLocations?serviceIds=${serviceIds}&locationType=${locationType}&searchTerm=${searchTerm}`);
   }
 }
