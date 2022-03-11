c2f46f4d62beb861a6854b70cda7af9c18ad7fc4
Paul Howes <paul.howes@HMCTS.NET>
2022-03-11 13:33:47 +0000

    HMCTS Pagination component

---
+++ projects/exui-common-lib/src/lib/gov-ui/components/hmcts-pagination/hmcts-pagination.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'xuilib-hmcts-pagination',
+  templateUrl: './hmcts-pagination.component.html',
+  styleUrls: ['./hmcts-pagination.component.scss']
+})
+export class HmctsPaginationComponent {
+
+  @Input() public id: string;
+  @Input() public maxSize: number = 7;
+  @Input() public pageSize: number = 10;
+  @Input() public vshowPageNumbers: boolean = true;
+  @Input() public showResultCount: boolean = true;
+  @Output() public pageChange: EventEmitter<number> = new EventEmitter<number>();
+  @Output() public pageBoundsCorrection: EventEmitter<number> = new EventEmitter<number>();
+
+}
