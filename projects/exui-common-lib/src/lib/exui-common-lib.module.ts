3ae2093a7760d14b534f34327a1f73717a00d6e1
John Benjamin <john.benjamin@hmcts.net>
2022-05-03 16:57:16 +0100

    Angular 9 Test fixes

--- projects/exui-common-lib/src/lib/exui-common-lib.module.ts
+++ projects/exui-common-lib/src/lib/exui-common-lib.module.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { RouterModule } from '@angular/router';
 import { AccessibilityComponent } from './components/accessibility/accessibility.component';
 import { CheckboxListComponent } from './components/checkbox-list/checkbox-list.component';
