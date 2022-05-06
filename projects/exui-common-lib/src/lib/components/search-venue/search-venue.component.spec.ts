9c693dfaf89b285753d69ad8de6f15a24abe4eff
Phillip Whitaker <phillip.whitaker1@hmcts.net>
2022-05-06 11:23:19 +0100

    fixed tests

--- projects/exui-common-lib/src/lib/components/search-venue/search-venue.component.spec.ts
+++ projects/exui-common-lib/src/lib/components/search-venue/search-venue.component.spec.ts
@@ -1,6 +1,7 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {ReactiveFormsModule} from '@angular/forms';
-import {MatAutocompleteModule, MatOptionModule} from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatOptionModule } from '@angular/material/core';
 import {By} from '@angular/platform-browser';
 import {RouterTestingModule} from '@angular/router/testing';
 import {of} from 'rxjs';
