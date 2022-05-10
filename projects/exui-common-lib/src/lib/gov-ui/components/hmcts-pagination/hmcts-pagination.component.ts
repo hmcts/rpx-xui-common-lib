71b2c80026adc575ea3f90eb3ead1f367eb487b7
Philip Whitaker <phillip.whitaker1@hmcts.net>
2022-05-10 11:56:46 +0100

    reverted some console logs

--- projects/exui-common-lib/src/lib/gov-ui/components/hmcts-pagination/hmcts-pagination.component.ts
+++ projects/exui-common-lib/src/lib/gov-ui/components/hmcts-pagination/hmcts-pagination.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'xuilib-hmcts-pagination',
   templateUrl: './hmcts-pagination.component.html',
   styleUrls: ['./hmcts-pagination.component.scss']
 })
-export class HmctsPaginationComponent implements OnInit {
+export class HmctsPaginationComponent {
 
   @Input() public id: string;
   @Input() public maxSize: number = 7;
@@ -15,8 +15,4 @@ export class HmctsPaginationComponent implements OnInit {
   @Output() public pageChange: EventEmitter<number> = new EventEmitter<number>();
   @Output() public pageBoundsCorrection: EventEmitter<number> = new EventEmitter<number>();
 
-  public ngOnInit(): void {
-    console.log('HmctsPaginationComponent');
-    console.log(this);
-  }
 }
