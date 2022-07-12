28a874eea22a0f6f5d8f8279913297557cd12aaf
Surekha-HMCTS <surekha.athikesavan@hmcts.net>
2022-07-12 09:39:49 +0100

    EUI-6097: taskNames API is included in sessionStorage

--- projects/exui-common-lib/src/lib/services/task-name/task-name.service.ts
+++ projects/exui-common-lib/src/lib/services/task-name/task-name.service.ts
@@ -1,19 +1,28 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { SessionStorageService } from '../session-storage/session-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskNameService {
+  public static taskNamesKey: string = 'taskNames';
 
-  constructor(private readonly http: HttpClient) {
+  constructor(private readonly http: HttpClient, private readonly sessionStorageService: SessionStorageService) {
   }
 
   /**
    * @return Observable<any[]>: Array of taskName in Observable
    */
   public getTaskName(): Observable<any[]> {
-    return this.http.get<any[]>(`/workallocation2/taskNames`);
+    if (this.sessionStorageService.getItem(TaskNameService.taskNamesKey)) {
+      const taskNames = JSON.parse(this.sessionStorageService.getItem(TaskNameService.taskNamesKey));
+      return of(taskNames as any[]);
+    }
+    return this.http.get<any[]>(`/workallocation2/taskNames`).pipe(
+      tap(taskNames => this.sessionStorageService.setItem(TaskNameService.taskNamesKey, JSON.stringify(taskNames)))
+    );
   }
 }
