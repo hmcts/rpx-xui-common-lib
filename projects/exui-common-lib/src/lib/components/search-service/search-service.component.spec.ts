b5a1b17aaed2e41b34ed8c396180daaecec0d766
Aamir Saeed <Aamir.Saaed@innovation.group>
2022-10-13 13:00:53 +0100

    Added an event of onSelectionChanged to get the latest selected option

--- projects/exui-common-lib/src/lib/components/search-service/search-service.component.spec.ts
+++ projects/exui-common-lib/src/lib/components/search-service/search-service.component.spec.ts
@@ -1,20 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatAutocompleteModule, MatOptionModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FilterConfigOption } from '../../models';
 import { SearchServiceComponent } from './search-service.component';
-
 describe('SearchServiceComponent', () => {
   let component: SearchServiceComponent;
   let fixture: ComponentFixture<SearchServiceComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, RouterTestingModule.withRoutes([]), MatAutocompleteModule, MatOptionModule],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes([]),
+        MatAutocompleteModule,
+        MatOptionModule,
+        BrowserAnimationsModule,
+      ],
       declarations: [SearchServiceComponent],
-      providers: []
-    })
-    .compileComponents();
+      providers: [],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -24,5 +31,24 @@ describe('SearchServiceComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit an event of serviceChanged', () => {
+    spyOn(component.serviceChanged, 'emit');
+    component.services.push(
+      {
+        key: '01',
+        label: '01-label',
+      },
+      {
+        key: '02',
+        label: '02-label',
+      }
+    );
+    component.onSelectionChanged('01');
+    expect(component.serviceChanged.emit).toHaveBeenCalledWith({
+      key: '01',
+      label: '01-label',
+    } as FilterConfigOption);
   });
 });
