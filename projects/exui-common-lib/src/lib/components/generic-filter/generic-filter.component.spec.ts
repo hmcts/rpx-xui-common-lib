323273d8da4ed30f0dc740fc26847ae77f17961b
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2023-01-31 12:04:45 +0000

    Push fixes to tests

--- projects/exui-common-lib/src/lib/components/generic-filter/generic-filter.component.spec.ts
+++ projects/exui-common-lib/src/lib/components/generic-filter/generic-filter.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule, MatOptionModule } from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatOptionModule } from '@angular/material/core';
 import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
 import { FilterFieldConfig } from '../../models';
