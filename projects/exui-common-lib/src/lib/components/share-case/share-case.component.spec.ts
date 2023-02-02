0f6828f2a9abac39b0eeb650b28b9e20d1756278
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2023-02-02 17:00:15 +0000

    Fix test issue

--- projects/exui-common-lib/src/lib/components/share-case/share-case.component.spec.ts
+++ projects/exui-common-lib/src/lib/components/share-case/share-case.component.spec.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
