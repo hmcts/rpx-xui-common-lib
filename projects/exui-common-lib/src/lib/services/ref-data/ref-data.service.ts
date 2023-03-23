3eaf75df370327c1036ee51d4f4907cfdaf1f8d1
Matei Baciu <matei.baciu@hmcts.net>
2023-03-23 17:45:53 +0000

    added getLocationsByServiceCode

--- projects/exui-common-lib/src/lib/services/ref-data/ref-data.service.ts
+++ projects/exui-common-lib/src/lib/services/ref-data/ref-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { forkJoin, Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { map, shareReplay, take } from 'rxjs/operators';
 import { RefDataHMCTSService } from './models/ref-data-htmcs-service.model';
 import { RefDataLocation } from './models/ref-data-location.model';
 import { RefDataRegion } from './models/ref-data-region.model';
@@ -12,29 +12,29 @@ import { RefDataDataAccessService } from './ref-data-data-access/ref-data-data-a
 export class RefDataService {
   public regions$: Observable<RefDataRegion[]>;
   public services$: Observable<RefDataHMCTSService[]>;
-
-  private locationsByServiceCodes: {[serviceCode: string]: Observable<any>} = {};
+  private readonly locationsByServiceCodesCache: {[serviceCode: string]: Observable<RefDataLocation[]>} = {};
 
   constructor(private refDataDataAccessService: RefDataDataAccessService) {
     this.regions$ = this.refDataDataAccessService.getRegions().pipe(shareReplay());
     this.services$ = this.refDataDataAccessService.getServices().pipe(shareReplay());
   }
 
-  public getLocationsByServiceCodes(serviceCodes: string[]): Promise<RefDataLocation[]> {
+  public getLocationsByServiceCodes(serviceCodes: string[]): Observable<RefDataLocation[]> {
     const observables: Observable<RefDataLocation[]>[] = [];
 
     serviceCodes.forEach(serviceCode => {
-      if (!this.locationsByServiceCodes[serviceCode]) {
-        this.locationsByServiceCodes[serviceCode] = this.refDataDataAccessService.getLocationsByServiceCode(serviceCode).pipe(
+      if (!this.locationsByServiceCodesCache[serviceCode]) {
+        this.locationsByServiceCodesCache[serviceCode] = this.refDataDataAccessService.getLocationsByServiceCode(serviceCode).pipe(
           map(responseData => responseData.court_venues),
           shareReplay()
         );
       }
-      observables.push(this.locationsByServiceCodes[serviceCode]);
+      observables.push(this.locationsByServiceCodesCache[serviceCode]);
     });
 
     return forkJoin(observables).pipe(
       map((values) => values.reduce((acc, val) => acc.concat(val)), []),
-    ).toPromise();
+      take(1)
+    );
   }
 }
