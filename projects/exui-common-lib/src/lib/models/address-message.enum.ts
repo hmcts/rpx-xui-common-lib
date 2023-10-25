5b932915333e9bbddaaca4b20ebe8344798ceaed
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2023-10-25 17:06:37 +0100

    Make required error message changes

--- projects/exui-common-lib/src/lib/models/address-message.enum.ts
+++ projects/exui-common-lib/src/lib/models/address-message.enum.ts
@@ -1,9 +1,10 @@
 export enum AddressMessageEnum {
-    NO_OPTION_SELECTED = 'Please select an answer',
+    NO_OPTION_SELECTED = 'Select an option',
     NO_STREET_SELECTED = 'Enter building and street',
     NO_CITY_SELECTED = 'Enter town or city',
     NO_COUNTRY_SELECTED = 'Enter country',
     NO_POSTCODE_SELECTED = 'Enter a valid postcode',
     SELECT_ADDRESS = 'Select an address',
-    INVALID_POSTCODE = 'Enter valid postcode'
+    INVALID_POSTCODE = 'Enter valid postcode',
+
   }
