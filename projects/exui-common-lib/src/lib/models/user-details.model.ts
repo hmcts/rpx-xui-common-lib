fe1ddaf1607ceac1a5fb051448df5afc7b7e601a
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-12-04 13:08:40 +0000

    GA-31 update user details model to include access types and last updated

--- projects/exui-common-lib/src/lib/models/user-details.model.ts
+++ projects/exui-common-lib/src/lib/models/user-details.model.ts
@@ -1,7 +1,11 @@
+import { AccessTypeModel } from './access-type.model';
+
 export interface UserDetails {
   caseRoles?: string;
   email: string;
   firstName: string;
   idamId: string;
   lastName: string;
+  lastUpdated: Date;
+  accessTypes: AccessTypeModel[];
 }
