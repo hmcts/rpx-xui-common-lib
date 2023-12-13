9189ea468c223eb97074116e24c82d92b9da2e05
OgunyemiO <ola.ogunyemi@hmcts.net>
2023-12-13 18:37:04 +0000

    accessibility filter implementation

--- projects/exui-common-lib/src/lib/components/accessibility/accessibility.component.ts
+++ projects/exui-common-lib/src/lib/components/accessibility/accessibility.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'xuilib-accessibility',
-    templateUrl: './accessibility.component.html',
-    styleUrls: ['./accessibility.component.scss']
+  selector: 'xuilib-accessibility',
+  templateUrl: './accessibility.component.html',
+  styleUrls: ['./accessibility.component.scss']
 })
 
-export class AccessibilityComponent {
+export class AccessibilityComponent implements OnInit {
+  isEndpointMC: boolean;
   constructor() {
+  }
+
+  ngOnInit(): void {
+    const urlRef = window.location.href;
+    // pointing to MC & preview. May need to refactor this if needed for
+    // more applications
+    if (urlRef.includes('manage-case')
+      || urlRef.includes('xui-webapp')) {
+      this.isEndpointMC = true;
+    } else {
+      // pointing to MO. 
+      this.isEndpointMC = false;
+    }
   }
 }
