bc66445026e77c11bf0f96a9f06493b566772088
OgunyemiO <ola.ogunyemi@hmcts.net>
2024-04-23 17:13:45 +0100

    service banner message functionalities reworked with start and end dates

--- projects/exui-common-lib/src/lib/components/service-message/service-message.component.ts
+++ projects/exui-common-lib/src/lib/components/service-message/service-message.component.ts
@@ -1,16 +1,19 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ServiceMessages } from '../../models/service-message.model';
 
 @Component({
   selector: 'xuilib-service-message',
-  templateUrl: './service-message.component.html'
+  templateUrl: './service-message.component.html',
+  styleUrls: ['./service-message.component.scss']
 })
 export class ServiceMessageComponent {
-  @Input() public message: string;
-  @Input() public key: string;
-  @Output() public hideMessage = new EventEmitter<string>();
+  @Input() public message_en: string;
+  @Input() public message_cy?: string;
+  @Input() public key: ServiceMessages;
+  @Output() public hideMessage = new EventEmitter<ServiceMessages>();
   constructor() { }
 
-  public onHideMessageEvent(key: string) {
+  public onHideMessageEvent(key: ServiceMessages) {
     this.hideMessage.emit(key);
   }
 }
---
+++ projects/exui-common-lib/src/lib/models/service-message.model.ts
@@ -0,0 +1,8 @@
+export interface ServiceMessages {
+    index: number;
+    roles: string;
+    message_en: string;
+    message_cy: string;
+    begin: string;
+    end: string;
+  }
