82c67f384ca4b2a0f24f2cf1e31b280f689ca69c
OgunyemiO <ola.ogunyemi@hmcts.net>
2024-04-24 17:26:57 +0100

    Message filter rework. start date and end date added

--- projects/exui-common-lib/src/lib/components/service-message/service-message.component.ts
+++ projects/exui-common-lib/src/lib/components/service-message/service-message.component.ts
@@ -1,16 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ServiceMessages } from '../../models/service-message.model';
 
 @Component({
   selector: 'xuilib-service-message',
   templateUrl: './service-message.component.html'
 })
 export class ServiceMessageComponent {
-  @Input() public message: string;
-  @Input() public key: string;
-  @Output() public hideMessage = new EventEmitter<string>();
+  @Input() public message_en: string;
+  @Input() public message_cy?: string;
+  @Input() public key: ServiceMessages;
+  @Output() public hideMessage = new EventEmitter<ServiceMessages>();
   constructor() { }
 
-  public onHideMessageEvent(key: string) {
+  public onHideMessageEvent(key: ServiceMessages) {
     this.hideMessage.emit(key);
   }
 }
