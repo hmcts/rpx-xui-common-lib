779009fbd0750c9528e41e607b542f9b006d7083
OgunyemiO <ola.ogunyemi@hmcts.net>
2024-04-24 18:34:42 +0100

    Start date and end date added to Service Message

--- projects/exui-common-lib/src/lib/components/service-messages/service-messages.component.ts
+++ projects/exui-common-lib/src/lib/components/service-messages/service-messages.component.ts
@@ -1,9 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FeatureToggleService } from '../../services/feature-toggle/feature-toggle.service';
+import { ServiceMessages } from '../../models/service-message.model';
 
-export interface ServiceMessages {
-  [key: string]: string;
-}
 
 @Component({
   selector: 'xuilib-service-messages',
@@ -11,7 +9,7 @@ export interface ServiceMessages {
 })
 export class ServiceMessagesComponent implements OnInit {
   public hiddenBanners: string[];
-  public filteredMessages = new Map<string, string>();
+  public filteredMessages: ServiceMessages[] = [];
 
   @Input() public userRoles: string[];
   @Input() public featureToggleKey: string;
@@ -24,7 +22,7 @@ export class ServiceMessagesComponent implements OnInit {
   }
 
   public getServiceMessages(): void {
-    this.featureToggleService.getValue<ServiceMessages>(this.featureToggleKey, null)
+    this.featureToggleService.getValue<ServiceMessages[]>(this.featureToggleKey, null)
       .subscribe(messages => {
         if (!!messages) {
           this.createFilteredMessages(messages);
@@ -32,19 +30,39 @@ export class ServiceMessagesComponent implements OnInit {
       });
   }
 
-  private createFilteredMessages(messages: ServiceMessages): void {
+  private createFilteredMessages(messages: ServiceMessages[]): void {
     this.hiddenBanners = JSON.parse(window.sessionStorage.getItem(this.serviceMessageCookie)) || [];
-    Object.keys(messages).forEach(key => {
-      const regEx = new RegExp(key);
-      if (this.userRoles.some(e => regEx.test(e)) && this.hiddenBanners.indexOf(key) === -1) {
-        this.filteredMessages.set(key, messages[key]);
+
+    messages.forEach(key => {
+      const regEx = new RegExp(key.roles);
+      if (this.userRoles.some(e => regEx.test(e))) {
+        this.filteredMessages = messages.filter(msg => !this.hiddenBanners.includes(msg.message_en)
+          && this.compareDates(msg));
       }
     });
   }
 
-  public hideMessage(key: string): void {
-    this.filteredMessages.delete(key);
-    this.hiddenBanners.push(key);
+  private compareDates(msg: ServiceMessages): boolean {
+    let currentDateTime = new Date();
+    let beginDate = null;
+    let endDate = null;
+
+    if (!isNaN(Date.parse(msg.begin))) {
+      beginDate = new Date(msg.begin);
+    }
+    if (!isNaN(Date.parse(msg.end))) {
+      endDate = new Date(msg.end);
+    }
+
+    const beginDateOK = !msg.begin || (beginDate && beginDate < currentDateTime);
+    const endDateOK = !msg.end || (endDate && endDate >= currentDateTime);
+    return beginDateOK && endDateOK
+  }
+
+
+  public hideMessage(msg: ServiceMessages): void {
+    this.filteredMessages = this.filteredMessages.filter(f => f.index !== msg.index)
+    this.hiddenBanners.push(msg.message_en);
     window.sessionStorage.setItem(this.serviceMessageCookie, JSON.stringify(this.hiddenBanners));
   }
 }
