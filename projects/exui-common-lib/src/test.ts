3d5618b3fd2aaf12169ea51b0a4505ad0d0c9727
olusegun odunukan <olusegun.odunukan@hmcts.net>
2024-04-25 13:58:09 +0100

    Update test.ts

--- projects/exui-common-lib/src/test.ts
+++ projects/exui-common-lib/src/test.ts
@@ -7,7 +7,7 @@ import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-import 'core-js/es7/reflect';
+import 'core-js/es/reflect';
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
