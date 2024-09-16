0136a45b9fc776422c00510a5f6f0337517eb186
OgunyemiO <142920130+OgunyemiO@users.noreply.github.com>
2024-09-16 13:01:59 +0100

    Start date and end date added to Service Message Banner (#458)
    * Start date and end date added to Service Message
    
    * version number change
    
    * removed commented lines of code
    
    * removed translation from component
    
    * version number changed
    
    * unit test text changed
    
    * version updated
    
    * error handling message added
    
    * banner message errors modified
    
    * version number amended
    
    * error reworked and unit test
    
    * version change
    
    * AC04 AC05 AC06 and unit test covered
    
    * version number changed
    
    * error bug fix
    
    * version text change
    
    * message error bug fixes
    
    * version change
    
    * reworked greater than dates
    
    * unit test and version update
    
    * Update to release version number
    
    ---------
    
    Co-authored-by: RiteshHMCTS <74713687+RiteshHMCTS@users.noreply.github.com>
    Co-authored-by: Ritesh Dsouza <ritesh.dsouza@HMCTS.net>
    Co-authored-by: Josh <josh.glasgow@hmcts.net>

--- projects/exui-common-lib/src/lib/components/service-messages/service-messages.component.ts
+++ projects/exui-common-lib/src/lib/components/service-messages/service-messages.component.ts
@@ -1,9 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FeatureToggleService } from '../../services/feature-toggle/feature-toggle.service';
+import { ServiceMessages } from '../../models/service-message.model';
 
-export interface ServiceMessages {
-  [key: string]: string;
-}
 
 @Component({
   selector: 'xuilib-service-messages',
@@ -11,7 +9,10 @@ export interface ServiceMessages {
 })
 export class ServiceMessagesComponent implements OnInit {
   public hiddenBanners: string[];
-  public filteredMessages = new Map<string, string>();
+  public filteredMessages: ServiceMessages[] = [];
+  public isBannerError: boolean;
+  public bannerErrorMsgs: { message: string, index: number }[] = [];
+  public originalMessages: ServiceMessages[] = [];
 
   @Input() public userRoles: string[];
   @Input() public featureToggleKey: string;
@@ -24,27 +25,77 @@ export class ServiceMessagesComponent implements OnInit {
   }
 
   public getServiceMessages(): void {
-    this.featureToggleService.getValue<ServiceMessages>(this.featureToggleKey, null)
+    this.featureToggleService.getValue<ServiceMessages[]>(this.featureToggleKey, null)
       .subscribe(messages => {
+        this.originalMessages = messages;
         if (!!messages) {
           this.createFilteredMessages(messages);
         }
       });
   }
 
-  private createFilteredMessages(messages: ServiceMessages): void {
+  private createFilteredMessages(messages: ServiceMessages[]): void {
     this.hiddenBanners = JSON.parse(window.sessionStorage.getItem(this.serviceMessageCookie)) || [];
-    Object.keys(messages).forEach(key => {
-      const regEx = new RegExp(key);
-      if (this.userRoles.some(e => regEx.test(e)) && this.hiddenBanners.indexOf(key) === -1) {
-        this.filteredMessages.set(key, messages[key]);
+
+    messages.forEach(key => {
+      const regEx = new RegExp(key.roles);
+      if (this.userRoles.some(e => regEx.test(e))) {
+        this.filteredMessages = messages.filter(msg => !this.hiddenBanners.includes(msg.message_en)
+          && this.compareDates(msg));
       }
     });
   }
 
-  public hideMessage(key: string): void {
-    this.filteredMessages.delete(key);
-    this.hiddenBanners.push(key);
+  private compareDates(msg: ServiceMessages): boolean {
+    let currentDateTime = new Date();
+    let beginDate: Date | null = null;
+    let endDate: Date | null = null;
+    this.bannerErrorMsgs = [];
+
+    // Parse beginDate if msg.begin is present and valid
+    if (msg.begin && !isNaN(Date.parse(msg.begin))) {
+      beginDate = new Date(msg.begin);
+    }
+
+    if (msg.end && !isNaN(Date.parse(msg.end))) {
+      endDate = new Date(msg.end);
+    }
+    this.originalMessages.forEach(msg => {
+      // Only check for errors if both beginDate and endDate are present and valid
+      if ((msg.begin && !isNaN(Date.parse(msg.end))) && (msg.end && !isNaN(Date.parse(msg.end))) && new Date(msg.begin) > new Date(msg.end)) {
+        this.isBannerError = true;
+        this.bannerErrorMsgs.push({
+          message: `The start date is greater than the end date. Message index: ${msg.index}`,
+          index: msg.index
+        });
+      }
+      
+      // Check for invalid beginDate or endDate separately, if they are present
+      if (msg.begin && isNaN(Date.parse(msg.begin))) {
+        this.isBannerError = true;
+        this.bannerErrorMsgs.push({
+          message: `Invalid start date. Message index: ${msg.index}`,
+          index: msg.index
+        });
+      }
+      if (msg.end && isNaN(Date.parse(msg.end))) {
+        this.isBannerError = true;
+        this.bannerErrorMsgs.push({
+          message: `Invalid end date. Message index: ${msg.index}`,
+          index: msg.index
+        });
+      }
+    });
+
+    const beginDateOK = !msg.begin || (beginDate && beginDate < currentDateTime);
+    const endDateOK = !msg.end || (endDate && endDate >= currentDateTime);
+
+    return beginDateOK && endDateOK;
+  }
+
+  public hideMessage(msg: ServiceMessages): void {
+    this.filteredMessages = this.filteredMessages.filter(f => f.index !== msg.index)
+    this.hiddenBanners.push(msg.message_en);
     window.sessionStorage.setItem(this.serviceMessageCookie, JSON.stringify(this.hiddenBanners));
   }
 }
