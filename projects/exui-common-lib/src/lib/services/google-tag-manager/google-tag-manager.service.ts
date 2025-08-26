ea1d653a3c894b86781e7ea3c791afc862607767
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2025-08-26 16:01:10 +0100

    Revert google-tag-manager service changes

--- projects/exui-common-lib/src/lib/services/google-tag-manager/google-tag-manager.service.ts
+++ projects/exui-common-lib/src/lib/services/google-tag-manager/google-tag-manager.service.ts
@@ -1,87 +1,82 @@
 import { DOCUMENT } from '@angular/common';
-import { Inject, Injectable, Optional } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { NavigationEnd, Router } from '@angular/router';
-import { CSP_NONCE } from '@angular/core'; // <-- important
 import { windowToken } from '../../window';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class GoogleTagManagerService {
   private readonly document: Document;
   private readonly window: Window;
-  private loaded = false;
 
-  public googleTagManagerKey = '';
-
+  public googleTagManagerKey: string;
   constructor(
     private readonly router: Router,
     private readonly title: Title,
-    @Inject(windowToken) win: any,
-    @Inject(DOCUMENT) doc: any,
-    // note: the application of nonce is a safeguard, this should be handled by application
-    @Optional() @Inject(CSP_NONCE) private readonly cspNonce?: string
+    @Inject(windowToken) window: any,
+    @Inject(DOCUMENT) document: any
   ) {
-    this.window = win as Window;
-    this.document = doc as Document;
+    this.window = window as Window;
+    this.document = document as Document;
   }
 
-  /** Call this only AFTER the user has opted-in to analytics cookies */
   public init(googleTagManagerKey: string) {
-    console.log('Initializing Google Tag Manager');
-    if (this.loaded || !googleTagManagerKey) return;
-    this.loaded = true;
     this.googleTagManagerKey = googleTagManagerKey;
-
     try {
-      // 1) dataLayer bootstrap (executes as app code, not inline <script>)
       (this.window as any).dataLayer = (this.window as any).dataLayer || [];
       (this.window as any).dataLayer.push({
-        'gtm.start': Date.now(),
+        'gtm.start': new Date().getTime(),
         event: 'gtm.js'
       });
 
-      // 2) nonced external loader
       const script = this.document.createElement('script');
       script.async = true;
-      if (this.cspNonce) {
-        console.log('Applying CSP nonce to GTM script');
-        script.setAttribute('nonce', this.cspNonce);
-      }
-      script.src = `https://www.googletagmanager.com/gtm.js?id=${encodeURIComponent(this.googleTagManagerKey)}`;
-      script.addEventListener('error', (e) => {
-        // helpful to see CSP blocks in user consoles
-        // eslint-disable-next-line no-console
-        console.warn('[GTM] loader error', e);
-      });
+      script.src = `https://www.googletagmanager.com/gtm.js?id=${this.googleTagManagerKey}`;
       this.document.head.appendChild(script);
     } catch (ex) {
-      // eslint-disable-next-line no-console
-      console.error('Error appending Google Tag Manager', ex);
+      console.error('Error appending google tag manager');
+      console.error(ex);
     }
-
     this.listenForRouteChanges();
   }
 
   private listenForRouteChanges() {
-    if (!this.googleTagManagerKey) return;
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        (this.window as any).dataLayer?.push({
-          event: 'pageview',
-          page: {
-            path: event.urlAfterRedirects,
-            title: this.title.getTitle()
-          }
-        });
-      }
-    });
+    if (this.googleTagManagerKey) {
+      this.router.events.subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          (this.window as any).dataLayer.push({
+            event: 'pageview',
+            page: {
+              path: event.urlAfterRedirects,
+              title: this.title.getTitle()
+            }
+          });
+        }
+      });
+    }
   }
 
   public virtualPageView(eventName: string, path: string, title: string, metadata?: Record<string, any>) {
-    (this.window as any).dataLayer?.push({ event: eventName, page: { path, title }, ...metadata });
+    if ((this.window as any).dataLayer) {
+      (this.window as any).dataLayer.push({
+        event: eventName,
+        page: {
+          path,
+          title
+        },
+        ...metadata
+      });
+    }
   }
 
   public event(eventName: string, params: Record<string, any>) {
-    (this.window as any).dataLayer?.push({ event: eventName, ...params });
+    if ((this.window as any).dataLayer) {
+      (this.window as any).dataLayer.push({
+        event: eventName,
+        ...params
+      });
+    }
   }
 }
